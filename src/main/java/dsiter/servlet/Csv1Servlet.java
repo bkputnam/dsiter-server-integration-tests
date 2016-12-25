c8fe505f475dd2ff4c9bc630e639d08f99c67a5a
Brian Putnam <brian@brian-putnam.com>
2016-12-25 00:09:59 -0500

    Remove spaces from column names

--- src/main/java/dsiter/servlet/Csv1Servlet.java
+++ src/main/java/dsiter/servlet/Csv1Servlet.java
@@ -23,16 +23,16 @@ public class Csv1Servlet extends DatasetServlet {
 
 		// Transaction_date,Product,Price,Payment_Type,Name,City,State,Country,Account_Created,Last_Login,Latitude,Longitude
 		ColumnDescriptor[] columns = new ColumnDescriptor[] {
-			new ColumnDescriptor("Transaction date", ColumnType.STRING, -1),
+			new ColumnDescriptor("Transaction_date", ColumnType.STRING, -1),
 			new ColumnDescriptor("Product", ColumnType.STRING, -1),
 			new ColumnDescriptor("Price", ColumnType.INT, -1),
-			new ColumnDescriptor("Payment Type", ColumnType.STRING, -1),
+			new ColumnDescriptor("Payment_Type", ColumnType.STRING, -1),
 			new ColumnDescriptor("Name", ColumnType.STRING, -1),
 			new ColumnDescriptor("City", ColumnType.STRING, -1),
 			new ColumnDescriptor("State", ColumnType.STRING, -1),
 			new ColumnDescriptor("Country", ColumnType.STRING, -1),
-			new ColumnDescriptor("Account Created", ColumnType.STRING, -1),
-			new ColumnDescriptor("Last Login", ColumnType.STRING, -1),
+			new ColumnDescriptor("Account_Created", ColumnType.STRING, -1),
+			new ColumnDescriptor("Last_Login", ColumnType.STRING, -1),
 			new ColumnDescriptor("Latitude", ColumnType.DOUBLE, -1),
 			new ColumnDescriptor("Longitude", ColumnType.DOUBLE, -1)
 		};
